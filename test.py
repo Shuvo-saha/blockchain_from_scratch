95698b10076afb6554fce44ff4dbe55543673cdc
Shuvo Saha <shuvo.vertigo@gmail.com>
2021-03-01 15:16:14 +0600

    First commit. test.py still unfinished.

--- test.py
+++ test.py
@@ -5,19 +5,35 @@ from transaction import Transaction
 
 pp = pprint.PrettyPrinter(indent=4)
 blockchain = Blockchain()
+transactions = []
 
-input("Please type in Sender Name: ")
-input("Please type in Receiver Name: ")
-transaction = Transaction("Dan Rogers", "Shuvo", 155)
-transaction2 = Transaction("Shuvo", "Dan Rogers", 10)
-transaction3 = Transaction("Shuvo", "Martin", 10)
+while True:
+	sender = input("Please type in Sender Name: ")
+	receiver = input("Please type in Receiver Name: ")
+	amount = input("Please give the amount the sender will send: ")
+	transaction = Transaction(sender, receiver, amount)
+	transactions.append(transaction)
+	restart = input("Make another transaction? Please type Yes/No: ")
+	print(restart)
+	if restart == "Yes":
+		print("continuing")
+		pass
+	elif restart == "No":
+		print("exiting")
+		break
+	else:
+		print("invalid command")
+		break
 
-blockchain.unconfirmed_transactions.append(transaction) for transaction in transactions
+
+for one_transaction in transactions:
+	blockchain.unconfirmed_transactions.append(one_transaction)
 
 blockchain.mine_pending_transactions("darko")
 
 pp.pprint(blockchain.chain_JSON_encode())
 print("Length: ", len(blockchain.chain))
-print("Balance Shuvo: ", blockchain.get_balance("Shuvo"))
-print("Balance Dan Rogers: ", blockchain.get_balance("Dan Rogers"))
-print("Balance Martin: ", blockchain.get_balance("Martin"))
+
+for person in users:
+	person_amount = blockchain.get_balance(person)
+	print("Amount {person}: {person_amount}")
